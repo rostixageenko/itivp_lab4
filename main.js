1b7efe261e6950d05eca58ee6ffc814ff54753cc
rostixageenko <slavaageenko2005@gmail.com>
2025-04-27 14:05:24 +0300

    add validation

--- main.js
+++ main.js
@@ -3,6 +3,21 @@ const form = document.querySelector('#form');
 const input = document.querySelector('#input');
 const maxLength = 50;
 
+input.addEventListener('input',function(e){
+    input.value = input.value.replace(/[^а-яА-Яa-zA-ZёЁ\s]/g, '');
+    if (input.value.startsWith(' ')) {
+        input.value = input.value.trim();
+        alert('Ошибка: Ввод не должен начинаться с пробела.');
+    }
+
+    input.value = input.value.replace(/\s+/g, ' ');
+
+    if (input.value.length > maxLength) {
+        input.value = input.value.slice(0,maxLength );
+        alert('Ошибка: Длина ввода не должна превышать 50 символов.');
+    }
+});
+
 form.onsubmit = function (e) {
     e.preventDefault();
     let city = input.value.trim();
@@ -56,6 +71,6 @@ form.onsubmit = function (e) {
         })
         .catch((error) => {
             console.error('Произошла ошибка:', error);
-            document.querySelector('main').innerHTML = `<div class="error">${error.message}</div>`;
+            alert(`Ошибка: ${error.message}`);
         });
 };
